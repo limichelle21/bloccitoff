e8396d27e6e166aba4169aaa340f1d256e85f685
Michelle Li <limichelle21@gmail.com>
2016-01-18 19:24:55 -0800

    add confirmable option to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118200556) do
+ActiveRecord::Schema.define(version: 20160119025917) do
 
   create_table "items", force: :cascade do |t|
     t.text     "body"
