ed1c2611a1750f49c7a6dc4d5f43640ea392c4b1
Michelle Li <limichelle21@gmail.com>
2016-01-28 18:33:01 -0800

    updated item list view, added JS for AJAX, returning error on _item partial

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,23 @@ class ItemsController < ApplicationController
   # def update
   # end
 
-  # def destroy
-  # end
+  def destroy
+    @user = current_user
+    @item = @user.items.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "Item was completed!"
+    else
+      flash[:error] = "There was an error deleting the item."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+  end
+
 
   private
 
