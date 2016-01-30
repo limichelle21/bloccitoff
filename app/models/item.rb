40b7e52f2fe1ba7426eaee347c9481906970e19c
Michelle Li <limichelle21@gmail.com>
2016-01-29 19:53:53 -0800

    added days_left method to User show view

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,10 @@ class Item < ActiveRecord::Base
 	validates :completed, :inclusion => {:in => [true, false]}
 	validates :user, presence: true
 	
+	def days_left
+		7 - ((DateTime.now.to_date - created_at.to_date).to_i)
+	end
+
 
 
 end
