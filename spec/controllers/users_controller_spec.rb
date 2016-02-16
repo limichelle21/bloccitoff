64e7827b8bf6a85e84c21ec4929ca66c210fed14
Michelle Li <limichelle21@gmail.com>
2016-02-15 21:09:35 -0800

    updated item partial for show view

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,16 +7,12 @@ let(:my_user) { User.create!(name: RandomData.random_name, email: RandomData.ran
 
   describe "GET #show" do
     it "returns http success" do
-<<<<<<< HEAD
-      get :show, id: new_user_attributes.id
-=======
+
    	  sign_in(my_user)
       get :show, id: my_user.id
->>>>>>> automate-rake
+
       expect(response).to have_http_status(:success)
     end
   end
-
-
 
 end
